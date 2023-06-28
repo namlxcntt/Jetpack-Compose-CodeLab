c3911cb209045167781f4ec3567b498fef327f63
namlx <namlx@techcombank.com.vn>
2023-06-28 23:25:54 +0700

    Complete part 1

--- app/src/main/java/com/namlx/jetpackcomposecodelab1/MainActivity.kt
+++ app/src/main/java/com/namlx/jetpackcomposecodelab1/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.namlx.jetpackcomposecodelab1
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.namlx.jetpackcomposecodelab1.ui.theme.JetpackComposeCodeLab1Theme
@@ -29,12 +38,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApplication(modifier: Modifier = Modifier, names: List<String> = listOf("World","Compose")){
+private fun MyApplication(
+    modifier: Modifier = Modifier,
+) {
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
+
     Surface(modifier = modifier, color = MaterialTheme.colorScheme.background) {
-        Column(modifier) {
-            names.forEach {
-                Greeting(name = it)
-            }
+        if (shouldShowOnBoarding) {
+            OnBoardingScreen(onContinueClicked = {
+                shouldShowOnBoarding = false
+            })
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+fun Greetings(modifier: Modifier = Modifier, names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier) {
+        items(items = names) {
+            Greeting(name = it)
         }
     }
 }
@@ -44,18 +68,97 @@ private fun MyApplication(modifier: Modifier = Modifier, names: List<String> = l
  */
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colorScheme.primary, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
-        Column(modifier = Modifier.padding(24.dp).fillMaxWidth()) {
-            Text(text = "Hello,")
-            Text(text = name)
+
+    val expanded = rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            CardContent(name = name)
         }
     }
 }
 
-@Preview(showBackground = true, )
+@Composable
+private fun CardContent(name: String) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ){
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(bottom = 12.dp)
+        ) {
+            Text(text = "Hello,")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.ExtraBold)
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
+            }
+        }
+        IconButton(onClick = {
+            expanded = !expanded
+        }) {
+            Icon(imageVector = if (expanded) Filled.ExpandLess else Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                })
+        }
+    }
+}
+
+@Composable
+fun OnBoardingScreen(modifier: Modifier = Modifier, onContinueClicked: () -> Unit) {
+    val shouldShowOnBoarding by remember {
+        mutableStateOf(false)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to the basic codelab!")
+        Button(onClick = onContinueClicked, modifier = Modifier.padding(vertical = 24.dp)) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES, name = "Dark")
 @Composable
 private fun DefaultPreview() {
     JetpackComposeCodeLab1Theme {
-        MyApplication()
+        Greetings()
     }
 }
